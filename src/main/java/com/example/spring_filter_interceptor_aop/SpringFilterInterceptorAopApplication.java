8a8893fc4e4fd016d353911163df66b8c7613615
popo <kst005109@naver.com>
2023-05-05 16:05:36 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/com/example/spring_filter_interceptor_aop/SpringFilterInterceptorAopApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_filter_interceptor_aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFilterInterceptorAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFilterInterceptorAopApplication.class, args);
+    }
+
+}
